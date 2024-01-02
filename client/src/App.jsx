05913b741722d20a033b0dae3443ffd81ab49ffd
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-01-02 13:32:41 +0100

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from "./Signup";
+import Login from './Login';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
